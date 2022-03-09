c7d6cf0c73f4be17a1595c865089006704501614
Conor Kennedy <conor@conorkennedy.com>
2022-03-08 19:43:24 -0500

    figured out user input

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import fetch from 'node-fetch';
+import readlineSync from  'readline-sync';
 
+const cardName = readlineSync.question("Enter Card Name: ")
 
+var url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + cardName
 
-
-fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=Dark Magician')
+fetch(url)
 .then(response =>response.json())
 .then((cardData) => {
   var data = cardData.data['0']
   console.log(data.name)
+  console.log(data.desc)
 })
