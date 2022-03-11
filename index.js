9cd67345cbb35c7a4bcea32d09880e22b5335568
Conor Kennedy <conor@conorkennedy.com>
2022-03-11 09:45:41 -0500

    added message if ran without args

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 import fetch from 'node-fetch';
 
 const args = process.argv.slice(2);
-const cardName = args.toString().replace(',', " ");
+if(args.length === 0){
+  console.log("Enter a card as an argument")
+}else{
+  const cardName = args.toString().replace(',', " ");
 
-var url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + cardName;
+  var url = 'https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + cardName;
+  
+  fetch(url)
+  .then(response =>response.json())
+  .then((cardData) => {
+    var data = cardData.data['0']
+    console.log('\n')
+    console.log(data.name)
+    console.log(data.type)
+    console.log(data.desc)
+    console.log('atk: ' + data.atk)
+    console.log('def: ' + data.atk)
+  });
+}
 
-fetch(url)
-.then(response =>response.json())
-.then((cardData) => {
-  var data = cardData.data['0']
-  console.log('\n')
-  console.log(data.name)
-  console.log(data.type)
-  console.log(data.desc)
-  console.log('atk: ' + data.atk)
-  console.log('def: ' + data.atk)
-});
